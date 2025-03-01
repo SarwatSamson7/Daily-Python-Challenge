b1f4187b3c4330bd4fb42e388dfdcce7870be8fa
SarwatSamson7 <161697270+SarwatSamson7@users.noreply.github.com>
2025-03-01 20:47:09 +0500

    Add files via upload

---
+++ day_2/test.py
@@ -0,0 +1,8 @@
+# Daily python challenge - Day 2
+
+# Asking for the user's name
+name = input("Enter your name:")
+
+# Cool AI-Style welcome message
+print("\n processing... Name detected:", name)
+print(f"Acsess Granted! Welcome, Agent {name}. your mission awaits!")
